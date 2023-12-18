a037148cfe71b9d25d74a601820b00c9c0dd9402
Holmc555 <ogordienko528@gmail.com>
2023-12-18 13:11:28 +0200

    Initial commit

--- src/main/java/org/example/Mono.java
+++ src/main/java/org/example/Mono.java
@@ -14,18 +14,15 @@ import java.io.IOException;
 public class Mono {
 
     public static String getCurrencyRates(User user) {
-        // Створюємо HTTP клієнт
+
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            // Створюємо запит
+
             HttpGet request = new HttpGet("https://api.monobank.ua/bank/currency");
 
             try (CloseableHttpResponse response = httpClient.execute(request)) {
-                // Перевіряємо статус
                 if (response.getStatusLine().getStatusCode() == 200) {
-                    // Отримуємо тіло відповіді та конвертуємо в рядок
                     HttpEntity entity = response.getEntity();
                     String result = EntityUtils.toString(entity);
-                    // Обробляємо JSON відповідь
                     return processCurrencyData(result, user);
                 }
             }
@@ -36,23 +33,19 @@ public class Mono {
     }
 
     private static String processCurrencyData(String jsonData, User user) {
-        // Перетворювання рядка JSON на JSONArray
+
         JSONArray jsonArray = new JSONArray(jsonData);
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject currencyObject = jsonArray.getJSONObject(i);
-            // Отримати код валют
             int currencyCodeA = currencyObject.getInt("currencyCodeA");
             int currencyCodeB = currencyObject.getInt("currencyCodeB");
-
-            // Перевіряємо, чи підходить валюта під налаштування користувача
             if (currencyCodeB == 980 && (currencyCodeA == 840 || currencyCodeA == 978)) { // 840 - USD, 978 - EUR, 980 - UAH
                 String currencyName = currencyCodeA == 840 ? "Долар" : "Євро";
                 double rateBuy = currencyObject.getDouble("rateBuy");
                 double rateSell = currencyObject.getDouble("rateSell");
 
-                // Форматування
                 sb.append(currencyName)
                         .append(" - курс купівлі: ")
                         .append(String.format("%." + user.getCharsAfterComa() + "f", rateBuy))
